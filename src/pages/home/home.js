564aea85942255b30841ae730cd6e307a1f897d1
Natalia Tayoane <nataliatayoane@gmail.com>
2019-09-27 10:12:12 -0300

    Remoção do css não utilizado, ajuste no botão e comentários no que não está sendo utilizado no momento

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+/*eslist-disable 
+import ReactDOM from 'react-dom'; */
 import { Link } from 'react-router-dom';
 import './home.css';
 
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+/*eslist-disable 
+import ReactDOM from 'react-dom'; */
 import { Link } from 'react-router-dom';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './login.css';
