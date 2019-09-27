d14486c427db1ef6efacedb27608a4e177cf2c8e
Natalia Tayoane <nataliatayoane@gmail.com>
2019-09-26 22:52:24 -0300

    ajustando

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -6,34 +6,32 @@ import { Link } from 'react-router-dom';
 import './login.css';
 
 export default class Login extends React.Component {
-    render(){
+    render() {
         return (
-            
+
             <div className="Login">
 
                 <p className="Login-intro">
                     <Link to="/login"></Link>
                 </p>
 
-                <div className="fundo">
-
-                </div>
-
                 <form>
-                <div className="container">
-                    <label id="titulo">Entre em sua conta</label>
 
-                    <label>E-mail</label>
+                    <h1>Entre em sua conta</h1>
+                    <br />
+
+                    <label> E-mail</label>
                     <input className='input' type="text"></input>
 
                     <label> Senha</label>
-                    <input className='input'  type="password"></input>
+                    <input className='input' type="password"></input>
 
                     <span className="link1"><a href="#">Esqueceu sua senha?</a></span>
                     <span className="link2"><a href="#">Cadastre-se</a></span>
+                    <br/>
 
-                    <button type="submit"  className='btnEntrar'>Entrar</button>
-                </div>
+                    <button type="submit" className='btnEntrar'>Entrar</button>
+
                 </form>
             </div>
         );
