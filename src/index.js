19bbdd68630783c3350bdfb275831fddfc189cc3
Natalia Tayoane <nataliatayoane@gmail.com>
2019-09-30 17:11:28 -0300

    voltei ao que estava antes do último commit, comentei as alterações do mesmo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Login from './pages/login/login';
-// import Home from './pages/home/home'
-//import * as serviceWorker from './serviceWorker';
-//import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // ReactDOM.render(
 //     <BrowserRouter>
