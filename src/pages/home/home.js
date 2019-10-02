57f618104f8f292e0c616277eaa97d71ee840c1e
Natalia Tayoane <nataliatayoane@gmail.com>
2019-10-02 20:46:17 -0300

    atualizando

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -24,25 +24,45 @@ class Home extends React.Component {
 
                     <section styleName="container">
 
-                        <div class="row">
-                            <div class="col-3">
+                        <div className="row">
+                            <div className="col-3">
                                 <div styleName="bloco">
+                                    <span>
+                                        <p>Office 365</p>
+                                    </span>
 
+                                    <div styleName="img img-prod1"></div>
+
+                                    <div styleName="desc">
+                                        <p>Pacote de aplicativos para escritório</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <div styleName="bloco">
-
+                                    <p>VS Code</p>
+                                    <div styleName="img img-prod2"></div>
+                                    <div styleName="desc">
+                                        <p>Editor de código-fonte</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <div styleName="bloco">
-
+                                    <p>PhotoShop</p>
+                                    <div styleName="img img-prod3"></div>
+                                    <div styleName="desc">
+                                        <p>Editor de imagens bidimensionais do tipo raster</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-3">
+                            <div className="col-3">
                                 <div styleName="bloco">
-
+                                    <p>Oracle Database</p>
+                                    <div styleName="img img-prod4"></div>
+                                    <div styleName="desc">
+                                        <p>Oracle database 18c</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -50,31 +70,58 @@ class Home extends React.Component {
                     </section>
 
                     <section styleName="box">
-                        <div class="row">
-                            <div class="col-12">
+                        <div className="row">
+                            <div className="col-12">
                                 <div styleName="faixa">
+                                    <div className="row">
+                                        <div className="col-4">
+                                            <div styleName="line"></div>
+                                            <div styleName="desc">
+                                                <p><b>Todos os software que você precisa em um só lugar</b></p>
+                                                <p>Enviado pelo próprio fornecedor</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-4 cols">
+
+                                        </div>
+
+                                        <div className="col-4 cols">
+
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </section>
-{/* 
+
                     <section styleName="titulo">
-                        <div class="row">
-                            <div class="col-12">
-                                 <h4 id="ctg">Categoria Popular</h4>
+                        <div className="row">
+                            <div className="col-12">
+                                <h4>Categoria Popular</h4>
                             </div>
                         </div>
-                    </section> */}
+                    </section>
 
 
                     <section styleName="container-pop">
-                        <div class="row">
-                            <div class="col-12">
+                        <div className="row">
+                            <div className="col-12 ">
                                 <div styleName="pop">
                                 </div>
                             </div>
                         </div>
 
+                    </section>
+
+
+                    <section styleName="container-faixa">
+                        <div className="row">
+                            <div className="col-12">
+                                <div styleName="faixa">
+                                </div>
+                            </div>
+                        </div>
                     </section>
 
                 </div>
