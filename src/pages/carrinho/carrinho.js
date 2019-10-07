1dddb8fa0fac71a5b900159b4da9a9cda79d0b15
Natalia Tayoane <nataliatayoane@gmail.com>
2019-10-07 17:06:53 -0300

    paineis com aba da tela de carrinho

--- src/pages/carrinho/carrinho.js
+++ src/pages/carrinho/carrinho.js
@@ -11,29 +11,63 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Carrinho extends React.Component {
     render() {
-        return (
+        return (<>
+
+            <Menu />
             <div styleName="carrinho">
                 <div aria-label="breadcrumb" styleName="breadcrumb">
                     <ol className="breadcrumb">
                         <li className="breadcrumb-item"><a href="./">Home</a></li>
-                        <li className="breadcrumb-item"  aria-current="page">Meu carrinho</li>
+                        <li className="breadcrumb-item" aria-current="page">Meu carrinho</li>
                     </ol>
                 </div>
 
                 <div styleName="container">
                     <div className="row">
                         <div className="col-6" styleName="col1">
+                            <div styleName="car">
+                                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Carrinho</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#perfil" role="tab" aria-controls="profile" aria-selected="false">Salvos</a>
+                                    </li>
+                                </ul>
+                                <div class="tab-content" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+                                    <div class="tab-pane fade" id="perfil" role="tabpanel" aria-labelledby="profile-tab">jdnwjkdn</div>
+                                </div>
+                            </div>
 
-                        </div>
+</div>
+                            <div className="col-3" styleName="col2">
+                                <div styleName="titulo">
+                                    <p>Resumo do pedido</p>
+                                </div>
 
-                        <div className="col-4" styleName="col2">
+                                <div styleName="button">
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Aplicar cupom" aria-label="Aplicar cupom" aria-describedby="basic-addon2" />
+                                        <div class="input-group-append">
+                                            <button class="btn btn-outline-secondary" type="button">Aplicar</button>
+                                        </div>
+                                    </div>
+                                </div>
 
+                                <div styleName="total">
+                                    <p><b>Total: </b> R$199,99</p>
+                                </div>
+
+                                <div styleName="btnFinalizar">
+                                    <button>Finalizar</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+        </>);
+        }
     }
-}
-
-export default CSSModule(Carrinho, style, { allowMultiple: true, handleNotFoundStyleName: "ignore" })
+    
+export default CSSModule(Carrinho, style, {allowMultiple: true, handleNotFoundStyleName: "ignore" })
