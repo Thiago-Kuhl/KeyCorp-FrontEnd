3d37c60038654599ca68e02dca542277e9386f0c
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-10-08 20:45:55 -0300

    ta cadastrandooooooodasfddsfgfogdfhkogd

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -7,6 +7,54 @@ import { Link } from 'react-router-dom';
 import CSSModule from 'react-css-modules';
 import style from './login.module.css';
 class Login extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: '',
+            dtNasc: '',
+            cpf: '',
+            email: '',
+            password: ''
+        }
+    }
+    
+
+    handleChange = (event) => {
+        console.log("id",event.target.name)
+
+        const state = Object.assign({}, this.state);
+
+        let field = event.target.name;
+        
+        state[field] = event.target.value
+
+        this.setState(state)
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const signup = {
+            nome: this.state.nome,
+            dataNascimento: this.state.dtNasc,
+            cpf: this.state.cpf,
+            email: this.state.email,
+            senha: this.state.password
+        };
+
+        axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+        axios.post('http://35.237.84.170/signup/',  signup )
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(error => {
+                    return error;
+                }
+            )
+    }
+
     render() {
         return (
 
