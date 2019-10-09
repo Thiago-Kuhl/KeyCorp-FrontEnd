42f63e5eecf90f9f9752ea53515ea3f81ef62f46
Natalia Tayoane <nataliatayoane@gmail.com>
2019-10-08 21:26:31 -0300

    tela de carrinho, finalizando...

--- src/pages/carrinho/carrinho.js
+++ src/pages/carrinho/carrinho.js
@@ -14,6 +14,7 @@ class Carrinho extends React.Component {
         return (<>
 
             <Menu />
+
             <div styleName="carrinho">
                 <div aria-label="breadcrumb" styleName="breadcrumb">
                     <ol className="breadcrumb">
@@ -26,48 +27,65 @@ class Carrinho extends React.Component {
                     <div className="row">
                         <div className="col-6" styleName="col1">
                             <div styleName="car">
-                                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#Carrinho" role="tab" aria-controls="home" aria-selected="true">Carrinho</a>
+                                <ul className="nav nav-tabs" id="myTab" role="tablist">
+                                    <li className="nav-item">
+                                        <a className="nav-link active" id="home-tab" data-toggle="tab" href="#Carrinho" role="tab" aria-controls="home" aria-selected="true">Carrinho</a>
                                     </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#Salvos" role="tab" aria-controls="profile" aria-selected="false">Salvos</a>
+                                    <li className="nav-item">
+                                        <a className="nav-link" id="profile-tab" data-toggle="tab" href="#Salvos" role="tab" aria-controls="profile" aria-selected="false">Salvos</a>
                                     </li>
                                 </ul>
-                                <div class="tab-content" id="myTabContent">
-                                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-                                    <div class="tab-pane fade" id="perfil" role="tabpanel" aria-labelledby="profile-tab">jdnwjkdn</div>
-                                </div>
-                            </div>      
+                                <div className="tab-content" id="myTabContent">
+                                    <div className="tab-pane fade show active" id="carinho" role="tabpanel" aria-labelledby="home-tab">
+                                        <div styleName="imagem-carrinho"> </div>
 
-</div>
-                            <div className="col-3" styleName="col2">
-                                <div styleName="titulo">
-                                    <p>Resumo do pedido</p>
-                                </div>
+                                        <div styleName="info-carrinho">
+                                            <span>
+                                                <h3><b>Ebook Marketing Digital</b></h3>
+                                            </span>
 
-                                <div styleName="button">
-                                    <div class="input-group mb-3">
-                                        <input type="text" class="form-control" placeholder="Aplicar cupom" aria-label="Aplicar cupom" aria-describedby="basic-addon2" />
-                                        <div class="input-group-append">
-                                            <button class="btn btn-outline-secondary" type="button">Aplicar</button>
+                                            <span styleName="desc">
+                                                <h3>Ações de comunicação que as empresas podem utilizar por meio da internet</h3>
+                                            </span>
                                         </div>
+
+                                        <div styleName="iptCar">
+                                            <input type="number" placeholder="1" />
+                                        </div>
+
+                                    </div>
+                                    <div className="tab-pane fade" id="salvos" role="tabpanel" aria-labelledby="profile-tab">jdnwjkdn</div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="col-3" styleName="col2">
+                            <div styleName="titulo">
+                                <p>Resumo do pedido</p>
+                            </div>
+
+                            <div styleName="button">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Aplicar cupom" aria-label="Aplicar cupom" aria-describedby="basic-addon2" />
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="button">Aplicar</button>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div styleName="total">
-                                    <p><b>Total: </b> R$199,99</p>
-                                </div>
+                            <div styleName="total">
+                                <p><b>Total: </b> R$199,99</p>
+                            </div>
 
-                                <div styleName="btnFinalizar">
-                                    <button>Finalizar</button>
-                                </div>
+                            <div styleName="btnFinalizar">
+                                <button>Finalizar</button>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </>);
-        }
     }
-    
-export default CSSModule(Carrinho, style, {allowMultiple: true, handleNotFoundStyleName: "ignore" })
+}
+
+export default CSSModule(Carrinho, style, { allowMultiple: true, handleNotFoundStyleName: "ignore" })
