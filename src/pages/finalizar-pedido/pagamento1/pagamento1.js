fa25052eebed8a3265d7be0b749b17ed8549ed69
thiago-kuhl <thiago.kuhl15@gmail.com>
2019-12-09 17:38:42 -0300

    Adicionando validador de bin

--- src/pages/finalizar-pedido/pagamento1/pagamento1.js
+++ src/pages/finalizar-pedido/pagamento1/pagamento1.js
@@ -10,7 +10,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'react-bootstrap';
 import axios from 'axios';
 import Cookies from 'universal-cookie';
-
+import mastercard from '../../../image/mastercard-logo.png'
+import visa from '../../../image/visa-logo.png'
 
 class Pagamento1 extends React.Component {
 
@@ -23,6 +24,58 @@ class Pagamento1 extends React.Component {
             codigo: ''
         }
     }
+
+    validadeExpirationDate = (event) =>{
+        
+        let date = new Date;
+        let year = date.getFullYear().toString().substr(-2);
+
+        let expirationDate = document.getElementById("vencimento").value
+
+        let expirationMonth = expirationDate[0] + expirationDate[1]
+        let expirationYear = expirationDate[2] + expirationDate[3]
+        
+        if(expirationMonth > 12 || expirationYear < year){
+            alert("Data do cartão inválida!")
+        }        
+    }
+
+    validadeCardBin = (event) => {
+        var cardNumbers = document.getElementById("cartao").value
+    
+            const url = 'https://lookup.binlist.net/43898481' /*+ cardNumbers[0]+cardNumbers[1]+cardNumbers[2]+cardNumbers[3]+cardNumbers[4]
+            +cardNumbers[5]+cardNumbers[6]+cardNumbers[7]*/
+
+        axios.defaults.headers.get['Accept-Version'] = '3';
+
+
+        axios.get(url)
+        .then( res => {
+          console.log(res.data);
+            var img;
+            //Recebe a bandeira do cartão de crédito
+            switch(res.data.scheme){
+
+                case "visa": 
+                img = visa;
+
+                break;
+
+                case "mastercard":
+                img = mastercard
+                break;
+
+                default: alert("Cartão inválido!")
+            } 
+            
+            
+        })
+        .catch(err => {
+            console.log(err);
+            
+        })
+    }
+
 
     processOrder = (event) => {
 
@@ -85,6 +138,19 @@ class Pagamento1 extends React.Component {
 
                 </div>
 
+                {/* <section styleName="container">
+
+                        <div className="row">
+                            <div className="col-3" onLoad={(event) => this.validadeCardBin(event)}>
+                               
+                                <div onClick={this.handleSubmit}  styleName="bloco">
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+                    </section> */}
+
+
                 <div styleName="container">
                     <form styleName="form">
 
@@ -93,14 +159,14 @@ class Pagamento1 extends React.Component {
                         <div styleName="wrapper1 wr">
                             <span>
                                 <label>Nome completo</label> <br />
-                                <input id="nome" type="text" placeholder="" ></input>
+                                <input id="nome" type="text" placeholder="Fulano da Silva" ></input>
                             </span>
                         </div>
 
                         <div styleName="wrapper2 wr">
                             <span>
                                 <label>Número do cartão</label> <br />
-                                <input maxLength="16" minLength="14" id="cartao" type="text"></input>
+                                <input maxLength="16" minLength="16" id="cartao" placeholder="1234 **** **** 5678" type="text" onBlur={(event) => this.validadeCardBin(event)}></input>
                             </span>
                         </div>
 
@@ -108,12 +174,12 @@ class Pagamento1 extends React.Component {
 
                             <span>
                                 <label>Vencimento</label> <br />
-                                <input id="vencimento" type="month" placeholder=""></input>
+                                <input maxLength="4" minLength="4" id="vencimento" type="month" placeholder="mm/yy" onBlur={(event) => this.validadeExpirationDate(event)} ></input>
                             </span>
 
                             <span>
                                 <label styleName="lb">Código</label> <br />
-                                <input maxLength="3" minLength="3" id="codigo" styleName="cdg" type="phone"></input>
+                                <input maxLength="3" minLength="3" id="codigo" styleName="cdg" placeholder="***" type="phone"></input>
                             </span>
 
                         </div>
